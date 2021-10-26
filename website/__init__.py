8a6f50da98cd8dc86cf5aa7f1288e34a5d2be0b4
SahilSayani <sahilsayani7@gmail.com>
2021-10-26 13:21:31 +0530

    users can log-in/signup successfully +added login reqd at home.html

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.helpers import url_for
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager, login_manager
 
 db=SQLAlchemy()
 DB_NAME="database.db"
@@ -19,8 +20,18 @@ def create_app():
     app.register_blueprint(auth,url_prefix='/')
     
     from .models import User,Note 
+    
     create_database(app)
     
+    login_manager=LoginManager()
+    login_manager.login_view='auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+
     return app
 
 def create_database(app):
--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint,render_template,request,flash,redirect,url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
-
+from flask_login import login_user,login_required,logout_user,current_user
 
 
 auth = Blueprint('auth',__name__)
@@ -16,15 +16,19 @@ def login():
         if user:
             if check_password_hash(user.password,password):
                 flash('Logged in successfully',category='success')
+                login_user(user,remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash("incorrect password,try again",category='error')   
     else:
-        flash('Username does not exist',category='error')             
-    return render_template("login.html")
+        flash('Email does not exist',category='error')             
+    return render_template("login.html",user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return"<p>logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
@@ -49,9 +53,9 @@ def sign_up():
             new_user=User(email=email,first_name=first_name,password=generate_password_hash(password,method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user,remember=True)
             flash('Account Created Successfully',category='success')
             
-            # return redirect(url_for('views.home'))
             #add user to Database
 
-    return render_template("register.html")
+    return render_template("register.html",user=current_user)
